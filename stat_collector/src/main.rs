0531d92a63daadd4955efc057758f90ff5c7ae56
adamski234 <adamski234@protonmail.com>
2024-01-25 15:17:15 +0100

    Allow integers in stats

--- stat_collector/src/main.rs
+++ stat_collector/src/main.rs
@@ -2,13 +2,14 @@ use std::os::unix::fs::MetadataExt;
 
 fn main() {
 	let header = "ants,iterations,evap_coeff,rand_chance,pher_weight,heur_weight,time_weight,graph_name,shortest_route,longest_route,average_route,time_for_stat_run";
-	let gex = regex::Regex::new(r"File (.*): .*is (\d*\.\d*),.*is (\d*\.\d*)\. .*is (\d*\.\d*)").unwrap();
-	//let gex = regex::Regex::new(r".*is (\d*\.\d*),.*is (\d*\.\d*)\. .*is (\d*\.\d*)").unwrap();
+	let gex = regex::Regex::new(r"File (.*): .*is (-?\d*\.?\d*),.*is (-?\d*\.?\d*)\. .*is (-?\d*\.?\d*)").unwrap();
+	//let gex = regex::Regex::new(r".*is (-?\d*\.?\d*),.*is (-?\d*\.?\d*)\. .*is (-?\d*\.?\d*)").unwrap();
 
 	println!("{}", header);
 
 	for filename in glob::glob("./output/*_run_data.csv").unwrap() {
 		let filename = filename.unwrap();
+		eprintln!("{}", filename.display());
 		let run_data = std::fs::read_to_string(&filename).unwrap();
 		let run_data = run_data.lines().nth(1).unwrap();
 		let stat_file = filename.to_str().unwrap().replace("run_data.csv", "statistics.txt");
